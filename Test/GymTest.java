44829cfe410e6fb8b02829f11787ff78d5e2c0c4
Venus Poon <venuspsw@hotmail.com>
2023-10-16 01:08:58 +0200

    Initial commit

--- Test/GymTest.java
+++ Test/GymTest.java
@@ -56,7 +56,7 @@ public class GymTest {
 
     @Test
     public void writeToFileTest(){
-        String outpath = "Test/outdata5";
+        String outpath = "Test/outdata";
         Person p = new Person("8204021234","Bear Belle","2012-12-12");
         Person p2 = new Person("2204021234","Bear2 Belle","2012-12-12");
         Person p3 = new Person("4204021234","Bear3 Belle","2012-12-12");
